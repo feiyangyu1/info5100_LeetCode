c90993d6a60aefc099bb6688715d5eae2a4ae376
12532 <yylm1111@gmail.com>
2021-10-23 18:36:30 -0700

    A2 Q10 SUBMITTED

--- Assignment2_Q10_reverseWordsInAString/src/Main.java
+++ Assignment2_Q10_reverseWordsInAString/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         String result = "";
         String[] arr = s.trim().split(" +");
         Collections.reverse(Arrays.asList(arr));
-        String.join(" ", arr);
+
         for (int i = 0; i < arr.length; i++) {
             if (i == arr.length - 1) {
                 result = result + " " + arr[i];
